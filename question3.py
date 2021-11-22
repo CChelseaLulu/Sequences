5f04af6a1e515df2c55c3a30c1686c67afad251e
CChelseaLulu <90858702+CChelseaLulu@users.noreply.github.com>
2021-11-22 22:26:38 +0300

    3

---
+++ question3.py
@@ -0,0 +1,9 @@
+def main():
+    first = str(input("Enter First number:"))
+    second = str(input("Enter Second number:"))
+    for x in first:
+        print(x)
+    for y in second:
+        print(y)
+        
+main()
