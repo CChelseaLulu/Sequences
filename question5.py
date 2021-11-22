c74b33ca75f9c1c78352d04dea76e94d608dec9d
CChelseaLulu <90858702+CChelseaLulu@users.noreply.github.com>
2021-11-22 22:27:10 +0300

    5

---
+++ question5.py
@@ -0,0 +1,17 @@
+def main():
+    grade = int(input("Enter grade(1-5):"))
+    if grade == 1:
+        print("Fail")
+    elif grade == 2:
+        print("Poor")
+    elif grade == 3:
+        print("Fair")
+    elif grade == 4:
+        print("Good")
+    elif grade == 5:
+        print("Excellent")
+    else:
+        print("Please enter a value between 1-5")
+
+main()
+                      
